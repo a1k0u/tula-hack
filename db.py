a1dbb07e654d83f1ba3af56d94a78db79054d3ef
a1k0u <alko7enko@gmail.com>
2022-04-24 12:46:13 +0300

    sql requests

--- db.py
+++ db.py
@@ -50,3 +50,42 @@ class DataBase:
             print("Ошибка получения данных из БД " + str(e))
 
         return False
+
+    def get_portfolio(self, username: str) -> dict:
+        try:
+            self.__cur.execute(f"SELECT * FROM portfolio WHERE username={username}")
+            general_info = self.__cur.fetchone()
+
+            self.__cur.execute(f"SELECT * FROM contacts WHERE username={username}")
+            contacts = self.__cur.fetchone()
+
+            self.__cur.execute(f"SELECT * FROM works WHERE username={username}")
+            works = self.__cur.fetchall()
+
+            self.__cur.execute(f"SELECT * FROM awards WHERE username={username}")
+            awards = self.__cur.fetchall()
+
+            self.__cur.execute(f"SELECT * FROM projects WHERE username={username}")
+            projects = self.__cur.fetchall()
+
+            self.__cur.execute(f"SELECT * FROM main_skills WHERE username={username}")
+            main_skills = self.__cur.fetchall()
+
+            self.__cur.execute(f"SELECT * FROM extra_skills WHERE username={username}")
+            extra_skills = self.__cur.fetchall()
+
+            self.__cur.execute(f"SELECT * FROM education WHERE username={username}")
+            education = self.__cur.fetchone()
+        except sqlite3.Error:
+            return {}
+
+        return {
+            **general_info,
+            **contacts,
+            **works,
+            **awards,
+            **projects,
+            **main_skills,
+            **extra_skills,
+            **education,
+        }
